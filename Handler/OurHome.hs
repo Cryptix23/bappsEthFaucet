d3880edd35cfbeb089e38cca6197e2add06eb8f5
Firescar96 <nchinda2@gmail.com>
2015-06-11 18:59:42 -0400

    First UI

--- Handler/OurHome.hs
+++ Handler/OurHome.hs
@@ -3,7 +3,8 @@ module Handler.OurHome where
 import Import
 
 getOurHomeR :: Handler Html
-getOurHomeR = defaultLayout $(widgetFile "OurHome")
+getOurHomeR = do
+  defaultLayout $(widgetFile "OurHome")
 
 postOurHomeR :: Handler Html
 postOurHomeR = error "Not yet implemented: postOurHomeR"
